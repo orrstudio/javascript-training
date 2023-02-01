ae4ae241207e8e61f5b5036678428a5a2b58d1e0
OrrArchPavilion <orr888@gmail.com>
2023-02-02 01:33:48 +0400

    vbnm

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = 30;
+num += 35;
+console.log(4 + "5");
+console.log(num);
+
+alert(num);
+confirm("nnnn");
+
+// https://www.youtube.com/watch?v=sQ9Oa-_7v-8&list=PLQEds1ESt7csjTThaq8xzdf0Axxhyk5mL&index=4
