1b06fe491eb702a5e48a9cdc284a96bfe75e6054
OrrArchPavilion <orr888@gmail.com>
2023-02-04 01:58:51 +0400

    lesson 5

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ let answers = [],
     }
     document.write(answers);
     console.log(answers);
+
+    let i = 0;
+    do {
+        console.log(i);
+        i++;
+        document.write(i);       
+    } while (i < 10);
+
+let u = 11;
+while (u < 100) {
+    console.log(u);
+    u++;
+    document.write(u); 
+}
