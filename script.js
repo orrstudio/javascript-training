e87232831b45be4a53938bd538eb4f49b5659f24
OrrArchPavilion <orr888@gmail.com>
2023-02-04 01:25:14 +0400

    Lesson 5

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let num = 30;
-num += 35;
-console.log(4 + "5");
-console.log(num);
+let answers = [],
+    questions = [
+        'How do you do?',
+        'Whats you name?',
+        'How old are you?'
+    ];
 
-alert(num);
-confirm("nnnn");
-
-// https://www.youtube.com/watch?v=sQ9Oa-_7v-8&list=PLQEds1ESt7csjTThaq8xzdf0Axxhyk5mL&index=4
+    for (let i = 0; i < questions.length; i++) {
+        answers[i] = prompt(questions[i], '');
+    }
+    document.write(answers);
+    console.log(answers);
